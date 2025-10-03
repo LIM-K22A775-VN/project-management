513120d02abb391bab8ed7d92899db4a2c01ee81
NguyenXuanTam <nxtl99@gmail.com>
2025-10-03 14:53:29 +0700

    lam trang danh sach san pham ben Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,12 +1,24 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index.pug",{
-        pageTitle : "Trang danh sach san pham"
-    }
-    );
-}// tem ham la index
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active", // tim nhung thang dang hoat dong
+        deleted: false // tim nhung thang chua XOA
+    });
+
+    const newProduct =  products.map( (item) => {
+        item.priceNew =  (item.price *(1 - item.discountPercentage/100)).toFixed(2); 
+        return item;
+    });
+
+    res.render("client/pages/products/index.pug", {
+        pageTitle: "Trang danh sach san pham",
+        products: newProduct
+    });
+} // tem ham la index
 module.exports.create = (req, res) => {
     res.render("client/pages/products/index.pug");
-}// tem ham la index
+} // tem ham la index
 module.exports.edit = (req, res) => {
     res.render("client/pages/products/index.pug");
-}// tem ham la edit
+} // tem ham la edit
