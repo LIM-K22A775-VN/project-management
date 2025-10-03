8ce5381a50790d47c87e838ce0dc9d6e497bd5a9
NguyenXuanTam <nxtl99@gmail.com>
2025-10-03 12:13:31 +0700

    Cau truc lai folder pug

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,7 @@
 module.exports.index = (req, res) => {
-    res.render("client/pages/home/index.pug");
+    res.render("client/pages/home/index.pug" , {
+        pageTitle : "Trang chủ"
+    });
 }// tem ham la index
 module.exports.create = (req, res) => {
     res.render("client/pages/home/index.pug");
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,8 @@
 module.exports.index = (req, res) => {
-    res.render("client/pages/products/index.pug");
+    res.render("client/pages/products/index.pug",{
+        pageTitle : "Trang danh sach san pham"
+    }
+    );
 }// tem ham la index
 module.exports.create = (req, res) => {
     res.render("client/pages/products/index.pug");
