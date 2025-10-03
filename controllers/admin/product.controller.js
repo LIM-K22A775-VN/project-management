3195a810ae20ef3faec2981fdd5eb62bd5a5e128
NguyenXuanTam <nxtl99@gmail.com>
2025-10-04 00:31:25 +0700

    Làm giao diện danh sách sản phẩm

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,11 @@
+module.exports.index = (req, res) => {
+    res.render("admin/pages/products/index.pug" , {
+        pageTitle : "Danh sách sản phẩm"
+    });
+}// tem ham la index
+module.exports.create = (req, res) => {
+    res.render("admin/pages/products/index.pug");
+}// tem ham la index
+module.exports.edit = (req, res) => {
+    res.render("admin/pages/products/index.pug");
+}// tem ham la edit
