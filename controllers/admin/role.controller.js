ed52bc9622d6949d0156a8e8618b73a023475a0c
NguyenXuanTam <nxtl99@gmail.com>
2025-10-21 00:30:10 +0700

    Xây dựng chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -3,7 +3,7 @@ const systemConfig = require("../../config/system.js")
 //[GET] /admin/roles
 module.exports.index = async (req, res)=>{
     let find = {
-        deleted : false,           
+        deleted : false,            
     };
 
     const records = await Role.find(find);
@@ -25,4 +25,33 @@ module.exports.createPost = async (req, res)=>{
     await record.save();
     req.flash('success', `Thêm danh mục thành công`);
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
+}
+
+//[GET] /admin/roles/edit
+module.exports.edit = async (req, res)=>{
+    let find ={
+        _id : req.params.id,
+        deleted:false
+    }
+    const record = await Role.findOne(find);
+    res.render("admin/pages/roles/edit",{
+        pageTitle: "Sửa nhóm quyền",
+        record : record
+    });
+}
+//[PATCH] /admin/roles/edit
+module.exports.editPatch = async (req, res)=>{
+    try {
+        const id = req.params.id;
+        await Role.updateOne({
+            _id: id
+        }, req.body);
+
+        req.flash('success', 'Cập nhật nhóm quyền thành công');
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    } catch (err) {
+        console.error('Lỗi khi cập nhật nhóm quyền', err);
+        req.flash('error', 'Có lỗi xảy ra khi cập nhật nhóm quyền');
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
 }
