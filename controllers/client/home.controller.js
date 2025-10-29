59215f46243aea9ca3fc84e9eba0d6f1e5423eea
NguyenXuanTam <nxtl99@gmail.com>
2025-10-29 22:38:31 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -7,11 +7,21 @@ module.exports.index = async (req, res) => {
         featured: "1",
         deleted: false,
         status: "active"
-    }).limit(2);
-    const newProduct = productHeplers.priceNewProducts(productsFeatured);
+    }).limit(6);
+    const newProductsFeatured = productHeplers.priceNewProducts(productsFeatured);
+    // Hết lấy ra sản phẩm nổi bật
+    const productsNew = await Product.find({
+
+        deleted: false,
+        status: "active"
+    }).sort({position : "desc"}).limit(6);
+    const newProductNew = productHeplers.priceNewProducts(productsNew);
+     // lấy ra sản phẩm mới nhất
+
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang chủ",
-        productsFeatured : newProduct      
+        productsFeatured : newProductsFeatured ,
+        productsNew :  newProductNew   
     });
 } // tem ham la index
 module.exports.create = (req, res) => {
