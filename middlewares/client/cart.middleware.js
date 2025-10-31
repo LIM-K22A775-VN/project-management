d85f1a64fa7a86c03e8806e02989341995e4e60d
NguyenXuanTam <nxtl99@gmail.com>
2025-10-31 21:15:46 +0700

    Làm nút minicart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -13,6 +13,15 @@ module.exports.cartId = async (req, res, next) => {
             expires : new Date(Date.now() + 365 * expiresCookies)
         });
     }else{
+        // nếu có giỏ thì lấy ra 
+        const cart = await Cart.findOne({
+            _id : req.cookies.cartId
+        });
+        const totalQuantity = cart.products.reduce((sum,item) =>
+            sum + item.quantity
+            , 0 ); // lấy tổng số lượng sản phẩm mà người dùng thêm vào giỏ hàng
+        cart.totalQuantity = totalQuantity;
+        res.locals.miniCart = cart;
 
     }
     next();
