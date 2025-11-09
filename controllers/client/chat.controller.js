544f542e9406aa6209a22560c0dee51098223d5e
NguyenXuanTam <nxtl99@gmail.com>
2025-11-09 23:56:21 +0700

    Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,39 @@
-module.exports.index = async (req, res) => {
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 
+module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id;
     //SocketIo
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
+    // on : cứ mỗi lần load thì lại vào
+    //once : load nhiều lần vẫn chỉ vào once 1 lần
+    _io.once('connection',(socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
+            // Lưu vào db 
+            const chat = new Chat(
+                {
+                user_id: userId,
+                content : content
+                }
+            );
+            await chat.save();
+        });
     });
     //SocketIo
+    // lấy data từ database
+    const chats = await Chat.find({
+        deleted : false
+    });
 
+    for (const chat of chats) {
+        const infoUser = await User.findOne({
+            _id : chat.user_id
+        }).select("fullName"); // lấy ra full Name
+        chat.infoUser = infoUser;
+    }
+    // console.log(chats);
+    // End lấy data từ database
     res.render("client/pages/chat/index", {
-        pageTitle: "Chat"
+        pageTitle: "Chat",
+        chats : chats
     })
 }
