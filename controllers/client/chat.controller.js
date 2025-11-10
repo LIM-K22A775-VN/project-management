0f634ee7c5f44ba701b8b88d5b15b00d0e3f028e
NguyenXuanTam <nxtl99@gmail.com>
2025-11-10 21:24:35 +0700

    Hiển thị data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,30 +3,39 @@ const User = require("../../models/user.model");
 
 module.exports.index = async (req, res) => {
     const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
     //SocketIo
     // on : cứ mỗi lần load thì lại vào
     //once : load nhiều lần vẫn chỉ vào once 1 lần
-    _io.once('connection',(socket) => {
-        socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
             // Lưu vào db 
-            const chat = new Chat(
-                {
+            const chat = new Chat({
                 user_id: userId,
-                content : content
-                }
-            );
+                content: content
+            });
             await chat.save();
+
+
+            // trả data về cho client
+            _io.emit("SEVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            });
+
         });
+
     });
-    //SocketIo
+    //End SocketIo
     // lấy data từ database
     const chats = await Chat.find({
-        deleted : false
+        deleted: false
     });
 
     for (const chat of chats) {
         const infoUser = await User.findOne({
-            _id : chat.user_id
+            _id: chat.user_id
         }).select("fullName"); // lấy ra full Name
         chat.infoUser = infoUser;
     }
@@ -34,6 +43,6 @@ module.exports.index = async (req, res) => {
     // End lấy data từ database
     res.render("client/pages/chat/index", {
         pageTitle: "Chat",
-        chats : chats
+        chats: chats
     })
 }
--- public/socket/chat.js
+++ public/socket/chat.js
@@ -12,3 +12,38 @@ if(formSendData){
     });
 }
 //END CLIENT_SEND_MESSAGE
+
+
+// SEVER_RETURN_MESSAGE
+socket.on("SEVER_RETURN_MESSAGE", (data)=>{
+    //id của user
+    const myId =  document.querySelector("[my-id]").getAttribute("my-id");
+
+
+    const innerBody = document.querySelector(".inner-body");
+    // console.log(data);
+    const div = document.createElement("div");
+    let htmlFullName ="";
+    if(data.userId === myId){
+        div.classList.add("inner-outgoing");
+    }else{
+        htmlFullName = `<div class="inner-name"> ${data.fullName} </div>`;
+        div.classList.add("inner-incoming");
+    }
+    div.innerHTML =`
+          ${htmlFullName}
+          <div class="inner-content"> ${data.content} </div>
+    `;
+    innerBody.appendChild(div);
+    innerBody.scrollTop = innerBodyChat.scrollHeight ;
+});
+// END SEVER_RETURN_MESSAGE
+
+//Sửa phần scoll cho tin nhắn xuống dưới cùng mỗi lúc load trang
+const innerBodyChat = document.querySelector(".inner-body");
+if(innerBodyChat){
+    innerBodyChat.scrollTop = innerBodyChat.scrollHeight ;
+     //scrollTop: cách top bao nhiêu = scrollHeight : chiều cao của toàn bộ tin nhắn scroll
+     //
+}
+//Sửa phần scoll cho tin nhắn xuống dưới cùng mỗi lúc load trang
